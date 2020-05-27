aedb4b4596ac2237acabe4d3be820c1af323074f
Mike Cooper <mythmon@gmail.com>
2020-05-27 09:20:14 -0700

    Store artifacts in CI jobs

---
+++ bin/pack-artifact.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eu
+
+# Prepare a directory for artifacts
+BUNDLE_NAME="rapid-experiments-shared-$(git describe --always)"
+OUTDIR="artifacts/$BUNDLE_NAME"
+mkdir -p $OUTDIR
+
+# Copy all artifacts into it
+cp -r ./schemas $OUTDIR
+
+# Bundle it
+tar -czvf $OUTDIR.tar.gz $OUTDIR
