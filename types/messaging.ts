84aa42e59da8895cdab42edc702fb474d923b73a
Andrei Oprea <andrei.br92@gmail.com>
2020-06-02 17:22:30 +0200

    Add CFR schema validation from mozilla-central
    Fixes #15

--- types/messaging.ts
+++ types/messaging.ts
@@ -1,9 +1,37 @@
-// TODO: This needs expanding
-interface MessageTrigger {
-  /** Name of the trigger */
-  id: string;
-  /** Additional parameters of the trigger */
-  params?: Array<string|number>
+import { TriggerAction } from "./triggerAction";
+import { MessagingAction } from "./specialMessageAction";
+
+type FluentId = { string_id: string };
+type FluentOrString = FluentId | string;
+
+/** Action dispatched by the button */
+interface CFRButton {
+  label: {
+    value: string;
+    attributes: {
+      /** A single character to be used as a shortcut key for the secondary button. This should be one of the characters that appears in the button label */
+      accesskey: string;
+    }
+  } | FluentId,
+  action: MessagingAction;
+}
+
+interface SimpleCFRContent {
+  /** The text that shows up in the chiclet. 20 characters max. */
+  notification_text: FluentOrString;
+  /** The background color of the chiclet as a HEX code. */
+  chiclet_color?: string;
+  heading_text: FluentOrString;
+  text: FluentOrString;
+  /** The icon displayed in the pop-over. Should be 32x32px or 64x64px and png/svg */
+  icon: string;
+  /** Dark theme variant of the icon if needed */
+  icon_dark_theme?: string;
+  /** The label and functionality for the buttons in the pop-over */
+  buttons: {
+    primary: CFRButton;
+    secondary: Array<CFRButton>;
+  }
 }
 
 export interface StandardMessageRecipe {
@@ -16,7 +44,7 @@ export interface StandardMessageRecipe {
   /** A JEXL expression representing targeting information */
   targeting?: string;
 
-  trigger?: MessageTrigger;
+  trigger?: TriggerAction;
 
   /** If more than one message is valid, the message with the higher priority will be shown  */
   priority?: number;
@@ -42,12 +70,11 @@ export interface StandardMessageRecipe {
   }
 
   /* Message-specific properties; each type of message template defines its own schema for content */
-  content: {[key: string]: any};
+  content: SimpleCFRContent;
 }
 
 export interface SimpleCFRMessage extends StandardMessageRecipe {
   template: "cfr_doorhanger";
-  // Triggers are requried for CFR, not optional
-  trigger: MessageTrigger;
-
+  // Triggers are required for CFR, not optional
+  trigger: TriggerAction;
 }
---
+++ types/specialMessageAction.ts
@@ -0,0 +1,127 @@
+export type MessagingAction =
+| DisableSTPDoorhangers
+| HighlightFeature
+| InstallAddonFromURL
+| OpenAboutPage
+| OpenApplicationsMenu
+| OpenAwesomeBar
+| OpenPreferencesPage
+| OpenPrivateBrowserWindow
+| OpenProtectionPanel
+| OpenProtectionReport
+| OpenUrlAction
+| PinCurrentTab
+| ShowFirefoxAccounts
+| ShowMigrationWizard
+| Cancel;
+
+/** Disables all STP doorhangers. */
+export interface DisableSTPDoorhangers {
+  type: "DISABLE_STP_DOORHANGERS";
+}
+
+/** Highlights an element, such as a menu item */
+export interface HighlightFeature {
+  data: {
+    /** The element to highlight */
+    args: string;
+  };
+  type: "HIGHLIGHT_FEATURE";
+}
+
+/** Install an add-on from AMO */
+export interface InstallAddonFromURL {
+  data: {
+    telemetrySource: string;
+    url: string;
+  };
+  type: "INSTALL_ADDON_FROM_URL";
+}
+
+/** Opens an about: page in Firefox */
+export interface OpenAboutPage {
+  data: {
+    /** The about page. E.g. "welcome" for about:welcome' */
+    args: string;
+    /** "Where the URL is opened, defaults to `tab` */
+    where: "current" | "save" | "tab" | "tabshifted" | "window";
+    /** Any optional entrypoint value that will be added to the search. E.g. "foo=bar" would result in about:welcome?foo=bar' */
+    entrypoint: string;
+  }
+  type: "OPEN_ABOUT_PAGE";
+}
+
+/** Opens an application menu */
+export interface OpenApplicationsMenu {
+  data: {
+    /** The menu name, e.g. "appMenu" */
+    args: string;
+  };
+  type: "OPEN_APPLICATIONS_MENU";
+}
+
+/** Focuses and expands the awesome bar */
+export interface OpenAwesomeBar {
+  type: "OPEN_AWESOME_BAR";
+};
+
+/** Opens a preference page */
+export interface OpenPreferencesPage {
+  data: {
+    /** Section of about:preferences, e.g. "privacy-reports" */
+    category: string;
+    /** Add a queryparam for metrics" */
+    entrypoint: string;
+  };
+  type: "OPEN_PREFERENCES_PAGE";
+}
+
+/** Opens a private browsing window. */
+export interface OpenPrivateBrowserWindow {
+  type: "OPEN_PRIVATE_BROWSER_WINDOW";
+}
+
+/** Opens the protections panel */
+export interface OpenProtectionPanel {
+  type: "OPEN_PROTECTION_PANEL";
+}
+
+/** Opens the protections panel report */
+export interface OpenProtectionReport {
+  type: "OPEN_PROTECTION_REPORT";
+}
+
+/** Opens given URL */
+export interface OpenUrlAction {
+  data: {
+    /** URL to open */
+    args: string;
+    /** Where the URL is opened, defaults to current tab */
+    where: "current" | "save" | "tab" | "tabshifted" | "window";
+  };
+  type: "OPEN_URL";
+}
+
+/** Pin the current tab */
+export interface PinCurrentTab {
+  type: "PIN_CURRENT_TAB",
+}
+
+/** Show Firefox Accounts */
+export interface ShowFirefoxAccounts {
+  type: "SHOW_FIREFOX_ACCOUNTS";
+  data: {
+    /** Adds entrypoint={your value} to the FXA URL */
+    entrypoint: string;
+  };
+}
+
+// Shows the Migration Wizard to import data from another Browser. See https://support.mozilla.org/en-US/kb/import-data-another-browser" */
+export interface ShowMigrationWizard {
+  type: "SHOW_MIGRATION_WIZARD";
+}
+
+/** Cancel (dismiss) the CFR doorhanger */
+export interface Cancel {
+  type: "CANCEL";
+}
