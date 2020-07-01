7f9a87839cb2cc8f2e2e1eb4a4c1e4b1f24523fb
Tim D. Smith <tdsmith@mozilla.com>
2020-06-30 17:25:48 -0700

    Find audience sizes with telemetry
    Providing SQL expressions in audience definitions that let us test group memberships will facilitate automatically sizing experiments.

--- data/Audiences.ts
+++ data/Audiences.ts
@@ -3,23 +3,32 @@ interface Audience {
   description: string;
   targeting?: string;
   filter_expression?: string;
+  /**
+   * A boolean SQL expression expressing whether a single row in telemetry.main would have
+   * matched the audience definition. These are used for sizing experiment populations,
+   * not for targeting.
+   */
+  desktop_telemetry?: string;
 }
 
 const audiences :  {[id: string] : Audience } = {
   all_english: {
     name: "All English users",
     description: "All users in en-* locales.",
-    targeting: "localeLanguageCode == \"en\""
+    targeting: "localeLanguageCode == \"en\"",
+    desktop_telemetry: "STARTS_WITH(environment.settings.locale, 'en')"
   },
   us_only: {
     name: "US users (en)",
     description: "All users in the US with an en-* locale.",
-    targeting: "localeLanguageCode == \"en\" && region == \"US\""
+    targeting: "localeLanguageCode == \"en\" && region == \"US\"",
+    desktop_telemetry: "STARTS_WITH(environment.settings.locale, 'en') AND normalized_country_code = 'US'"
   },
   first_run: {
     name: "First start-up users (en)",
     description: "First start-up users (e.g. for about:welcome) with an en-* locale.",
-    targeting: "localeLanguageCode == \"en\" && (isFirstStartup || currentExperiment.slug in activeExperiments) "
+    targeting: "localeLanguageCode == \"en\" && (isFirstStartup || currentExperiment.slug in activeExperiments) ",
+    desktop_telemetry: "STARTS_WITH(environment.settings.locale, 'en') AND payload.info.profile_subsession_counter = 1"
   }
 };
 
