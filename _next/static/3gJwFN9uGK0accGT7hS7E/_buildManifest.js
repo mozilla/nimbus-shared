db36a3e465a35950d7cc523b0d03a816e50baa91
ci-build <ci-build@nimbus.mozilla.org>
2020-07-21 17:13:05 +0000

    [skip ci] Docs updates

---
+++ _next/static/3gJwFN9uGK0accGT7hS7E/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/content/data":[a,b],"/content/schemas":[a,b]}}("static\u002Fchunks\u002F39ebb42b92214741b2fae9c4352c6caff21d0559.49c7d8dde257279023d0.js","static\u002Fcss\u002Fd33545502601c8a74270.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/756T4kYJFzO6LH5M4OBYJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/content/data":[a,b],"/content/schemas":[a,b]}}("static\u002Fchunks\u002Fabd73602947ed9151117edeae8c7a542f878df2e.4c2c4898a2627c8538c3.js","static\u002Fcss\u002Fd33545502601c8a74270.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
