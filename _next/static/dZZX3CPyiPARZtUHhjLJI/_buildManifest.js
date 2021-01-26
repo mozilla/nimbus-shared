9958f51ec7f167291c90e6e927b5ecea9e835162
ci-build <ci-build@nimbus.mozilla.org>
2021-01-26 21:36:37 +0000

    [skip ci] Docs updates

--- _next/static/dZZX3CPyiPARZtUHhjLJI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/content/data":[a,b],"/content/schemas":[a,b]}}("static\u002Fchunks\u002F4edd2a444e28ea8ad7830317a292d5c3472cd7ed.57eb85e819c94456f164.js","static\u002Fcss\u002Fd33545502601c8a74270.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
