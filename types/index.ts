59666e4a984bb5f578fdb6e13dc7dec826acf561
Barret Rennie <barret@brennie.ca>
2022-06-22 13:19:32 -0400

    Remove FxMS types from nimbus-shared
    The only consumers of these types are Fx Desktop, so they should live
    there instead.

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,4 @@
 export * as experiments from "./experiments";
-export * as messaging from "./messaging";
 export * as targeting from "./targeting";
 export * as normandy from "./normandy";
 export * as test from "./test";
