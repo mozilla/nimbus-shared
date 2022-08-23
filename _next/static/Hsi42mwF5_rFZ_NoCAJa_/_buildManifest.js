99ef3067117bdcbf5f99d7b32eaba2db31c5102b
ci-build <ci-build@nimbus.mozilla.org>
2022-08-23 22:25:44 +0000

    [skip ci] Docs updates

--- _next/static/Hsi42mwF5_rFZ_NoCAJa_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/content/data":[a,b],"/content/schemas":[a,b]}}("static\u002Fchunks\u002Fa6c7bc346ac11b52930d2fa39e78934644500779.00e760fe9faf38771855.js","static\u002Fcss\u002Fd33545502601c8a74270.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
